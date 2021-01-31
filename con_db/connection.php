a1b708bf727ca4dd8124b92271a6ce2afb294776
alif aziz <alif.aziz@outlook.com>
2021-01-31 19:20:58 +0700

    - git init

---
+++ con_db/connection.php
@@ -0,0 +1,5 @@
+<?php
+// $conn=mysqli_connect("localhost","u5398697_admin","sakura@89","u5398697_smpmuh");
+$conn=mysqli_connect("localhost","root","","db_smpmuh");
+date_default_timezone_set("Asia/Jakarta");date_default_timezone_set("Asia/Jakarta");
+?>
