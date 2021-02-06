534fd2b08426620f5ddc66e8cde323148e753944
alif aziz <alif.aziz@outlook.com>
2021-02-06 11:26:05 +0700

    - update connection.php

--- con_db/connection.php
+++ con_db/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect("localhost","ppdbmupa_admin","sakura@89","ppdbmupa_db_smpmuh");
+$conn=mysqli_connect("localhost","ppdbmupa_admin","141312@admin","ppdbmupa_db_smpmuh");
 // $conn=mysqli_connect("localhost","root","","db_smpmuh");
 date_default_timezone_set("Asia/Jakarta");date_default_timezone_set("Asia/Jakarta");
 ?>
